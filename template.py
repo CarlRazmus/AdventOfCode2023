51b7d26a762dc0b779d37336ad76203ef92cd7e4
CarlRazmus <carl.razmus@outlook.com>
2023-12-03 20:51:00 +0100

    add puzzle template file

---
+++ template.py
@@ -0,0 +1,12 @@
+import input_reader as ir
+
+def puzzle_1(input):
+    print("part1")
+
+def puzzle_2(input):
+    print("part2")
+
+if __name__ == "__main__":
+    input = ir.read_input_lines()
+    puzzle_1(input)
+    puzzle_2(input)
