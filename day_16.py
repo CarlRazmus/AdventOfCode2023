f7b55097b0f98d4c48d2f58df136c977d16b6fcf
CarlRazmus <carl.razmus@outlook.com>
2023-12-16 19:02:13 +0100

    improve day 16

--- day_16.py
+++ day_16.py
@@ -3,12 +3,12 @@ from collections import defaultdict
 
 
 beams = dict()
-traveled_pos = defaultdict(list)
+traveled_dirs = defaultdict(list)
 width = 0
 
 def puzzle_1(input):
     global beams
-    global traveled_pos
+    global traveled_dirs
     global width
     width = len(input)
     beams = dict()
@@ -19,74 +19,38 @@ def puzzle_1(input):
             count[(x, y)] = 0
 
     travel((1,0), (0,0))
-    print(len(traveled_pos))
+    print(len(traveled_dirs))
 
 def travel(dir, pos):
     global beams
-    global traveled_pos
+    global traveled_dirs
     global width
 
-    while pos[0] in range(width) and pos[1] in range(width) and dir not in traveled_pos[pos]:
-        traveled_pos[pos].append(dir)
+    while pos[0] in range(width) and pos[1] in range(width) and dir not in traveled_dirs[pos]:
+        traveled_dirs[pos].append(dir)
         beam = beams[pos]
 
-        if beam == ".":
+        if beam == "-" and dir[0] == 0:
+            travel((-1, 0), add_tuples(pos, (-1, 0)))
+            travel((1, 0), add_tuples(pos, (1, 0)))
+
+        elif beam == "|" and dir[1] == 0:
+            travel((0, -1), add_tuples(pos, (0, -1)))
+            travel((0, 1), add_tuples(pos, (0, 1)))
+
+        else:
+            if beam == "\\":
+                dir = (dir[1], dir[0])
+            elif beam == "/":
+                dir = (-dir[1], -dir[0])
             pos = add_tuples(pos, dir)
-
-        elif beam == "-":
-            if dir[1] == 0:
-                pos = add_tuples(pos, dir)
-            else:
-                travel((-1, 0), add_tuples(pos, (-1, 0)))
-                travel((1, 0), add_tuples(pos, (1, 0)))
-
-        elif beam == "|":
-            if dir[0] == 0:
-                pos = add_tuples(pos, dir)
-            else:
-                travel((0, -1), add_tuples(pos, (0, -1)))
-                travel((0, 1), add_tuples(pos, (0, 1)))
-
-        elif beam == "\\":
-            if dir == (1, 0):
-                dir = (0, 1)
-                pos = add_tuples(pos, dir)
-
-            elif dir == (-1, 0):
-                dir = (0, -1)
-                pos = add_tuples(pos, dir)
-
-            elif dir == (0, 1):
-                dir = (1, 0)
-                pos = add_tuples(pos, dir)
-
-            elif dir == (0, -1):
-                dir = (-1, 0)
-                pos = add_tuples(pos, dir)
-
-        elif beam == "/":
-            if dir == (1, 0):
-                dir = (0, -1)
-                pos = add_tuples(pos, dir)
-
-            elif dir == (-1, 0):
-                dir = (0, 1)
-                pos = add_tuples(pos, dir)
-
-            elif dir == (0, 1):
-                dir = (-1, 0)
-                pos = add_tuples(pos, dir)
-
-            elif dir == (0, -1):
-                dir = (1, 0)
-                pos = add_tuples(pos, dir)
 
 def add_tuples(t1, t2):
     return (t1[0] + t2[0], t1[1] + t2[1])
 
 def puzzle_2(input):
     global beams
-    global traveled_pos
+    global traveled_dirs
     global width
     width = len(input)
     beams = dict()
@@ -96,30 +60,30 @@ def puzzle_2(input):
 
     most_energized = 0
     for x in range(width):
-        traveled_pos.clear()
+        traveled_dirs.clear()
         travel((0,1), (x,0))
-        energized = len(traveled_pos)
+        energized = len(traveled_dirs)
         if energized > most_energized:
             most_energized = energized
 
     for x in range(width):
-        traveled_pos.clear()
+        traveled_dirs.clear()
         travel((0,-1), (x,width-1))
-        energized = len(traveled_pos)
+        energized = len(traveled_dirs)
         if energized > most_energized:
             most_energized = energized
 
     for y in range(width):
-        traveled_pos.clear()
+        traveled_dirs.clear()
         travel((1,0), (0, y))
-        energized = len(traveled_pos)
+        energized = len(traveled_dirs)
         if energized > most_energized:
             most_energized = energized
 
     for y in range(width):
-        traveled_pos.clear()
+        traveled_dirs.clear()
         travel((-1,0), (width-1, y))
-        energized = len(traveled_pos)
+        energized = len(traveled_dirs)
         if energized > most_energized:
             most_energized = energized
     print(most_energized)
